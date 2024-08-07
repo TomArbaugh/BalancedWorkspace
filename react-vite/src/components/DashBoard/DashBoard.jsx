8890b09bfa18774219e3d95da7555559ef70c0da
Thomas Arbaugh <151469077+TomArbaugh@users.noreply.github.com>
2024-08-07 13:08:08 -0700

    dashboard moble

--- react-vite/src/components/DashBoard/DashBoard.jsx
+++ react-vite/src/components/DashBoard/DashBoard.jsx
@@ -3,12 +3,13 @@ import {useDispatch, useSelector} from "react-redux"
 import { getAllTicketsThunk } from "../../redux/ticket"
 import { Link } from "react-router-dom"
 import { MdOutlineImageNotSupported } from "react-icons/md";
+import { useState } from "react";
 import "./DashBoard.css"
 
 function DashBoard () {
     
     const dispatch = useDispatch()
-
+    const [moble] = useState(window.innerWidth < 1310)
     const ticketState = useSelector((state) => state.ticket)
     let tickets; 
     ticketState ? tickets = ticketState.tickets : null
@@ -30,13 +31,13 @@ function DashBoard () {
     if (!tickets || Object.keys(ticketState) < 1) return null
 
     return (
-        <div>
+        <div id="whole-thing">
             <div className="dashboard-header">
             <h1>Dashboard</h1>
             <h4>({tickets.length}) Open Tickets</h4>
             </div>
             
-            <div id="view-tickets-header">
+            <div id={ !moble ? "view-tickets-header" : 'hide'}>
             <h3 className="ticket-headers">Id</h3>
             <h3 className="ticket-headers">Subject</h3>
             <h3 className="ticket-headers">Requester</h3>
@@ -53,11 +54,11 @@ function DashBoard () {
                     <Link 
                     to={`/view/ticket/${ticket.id}`}
                     className="view-ticket-preview">
-                    <h4 className="ticket-preview-element"># {ticket.id}</h4>
-                    <h4 className="ticket-preview-element">{ticket.title}</h4>
-                    <h4 className="ticket-preview-element">{ticket.requester}</h4>
-                    <h4 className="ticket-preview-element">{ticket.type}</h4>
-                    <h4 className="ticket-preview-element">{ticket.priority}</h4>
+                    <h4 className="ticket-preview-element">{moble ? <h3>ID:</h3>  : null} {ticket.id}</h4>
+                    <h4 className="ticket-preview-element">{moble ? <h3 id="subject">Subject:</h3> : null}{ticket.title}</h4>
+                    <h4 className="ticket-preview-element">{moble ? <h3>Requester:</h3> : null}{ticket.requester}</h4>
+                    <h4 className="ticket-preview-element">{moble ? <h3>Type:</h3> : null}{ticket.type}</h4>
+                    <h4 className="ticket-preview-element">{moble ? <h3>Priority:</h3> : null}{ticket.priority}</h4>
                     {ticket.tickets_images.length ? <img 
                     className="ticket-preview-image"
                     src={ticket.tickets_images[0].image} /> : <MdOutlineImageNotSupported />}

af83c6577b4d00388a92fb73fd14ec2614505fc3
Thomas Arbaugh <151469077+TomArbaugh@users.noreply.github.com>
2024-08-07 17:19:43 -0700

    resposive view ticket

--- react-vite/src/components/VIewTickets/ViewTickets.jsx
+++ react-vite/src/components/VIewTickets/ViewTickets.jsx
@@ -11,22 +11,22 @@ import "./ViewTickets.css"
 
 
 
-function ViewTickets(){
-    const {ticket_id} = useParams()
+function ViewTickets() {
+    const { ticket_id } = useParams()
     const dispatch = useDispatch()
-    
+
 
     useEffect(() => {
 
         dispatch(getTicketIdThunk(ticket_id))
 
     }, [dispatch, ticket_id])
-   // console.log(ticket_id)
+    // console.log(ticket_id)
     // console.log(ticket.title)
     useEffect(() => {
         dispatch(getAllMacrosThunk())
     }, [dispatch])
-    
+
     const customer = useSelector((state) => state.customer)
     const ticket = useSelector((state) => state.ticket.ticketById)
     const macros = useSelector((state) => state.applyMacro)
@@ -36,140 +36,140 @@ function ViewTickets(){
 
     }, [customer])
 
-    let requesterId; 
+    let requesterId;
     ticket ? requesterId = ticket.requester : null
 
-        useEffect(() => {
+    useEffect(() => {
 
-        }, [macros])
+    }, [macros])
 
-       useEffect(() => {
+    useEffect(() => {
 
         dispatch(getCustomerIdThunk(requesterId))
 
-       }, [dispatch, requesterId])
-      
+    }, [dispatch, requesterId])
 
 
-if (Object.keys(customer).length === 0 || !customer) return null 
-if (!ticket) return null
-if (!user) return null
-if (!macros) return  null
 
-// console.log("MACRO", macros)
-let chosenMacro;
-if (ticket.apply_macro) {
-   
-    macros ? chosenMacro = macros.macros.find((macro) => macro.id === ticket.apply_macro) : null
-} else {
-    chosenMacro = "No Macro Chosen"
-}
-// console.log("CHOSEN MAC", chosenMacro)
-return (
-    <div id="view-ticket-form">
-        <div id="view-ticket-top">
-            <div id="view-ticket-left">
-         <label>
-            <h4>Requester</h4>
-        
-<input
-className="view-ticket-requester"
-disabled={true}
-value={customer.customer ? customer.customer.name : null}
-/>
-    </label>
-    <p className="error-message"></p>
-    <label>
-        <h4> Assignee</h4>
-       
-<input
-className="view-ticket-assignee"
-disabled={true}
-value={user.username}
-/>
-    </label>
-    <div id="view-type-priority">
-    <label>
-        <h4>Type</h4>
-        
-<input 
-className="view-ticket-type"
-disabled={true}
-value={ticket.type}
-/>
-    </label>
-    <lable>
-        <h4>Priority</h4>
-        
-<input 
-className="view-ticket-priority"
-disabled={true}
-value={ticket.priority}
-/>
-    </lable>
-    </div>
-    </div>
-    <div id="view-ticket-middle">
-    <label>
-        <h4>Title</h4>
-        
-        <input
-        id="view-ticket-title-input"
-        value={ticket.title}
-        disabled={true}
-        />
-    </label>
-    <p className="error-message"> </p>
-   <lable> </lable>
-    <label>
-       
-        <h4>Description</h4>
-<input 
-id="view-ticket-description-input"
-disabled={true}
-value={ticket.description}
-/>
-    </label>
-    <p className="error-message"> </p>
-    </div>
-    <div id="view-ticket-right">
-    <label>
-        <h4>Image</h4>
-       
-        <img 
-        id="view-ticket-image"
-        src={ticket.tickets_images[0] ? ticket.tickets_images[0].image : null} />
-    </label>
-    </div>
-    </div>
-    <div id="view-ticket-bottom">
-    <lable className="view-ticket-macro">
-        <h4>Apply Macro</h4>
-        
-        <select
-        id="view-macros-input"
-        disabled={true}
-        value={chosenMacro ? chosenMacro.id : null}
-        >
-            <option>{chosenMacro ? chosenMacro.description : null}</option>
-        </select>
-    </lable>
-    <div className="view-delete-edit-buttons">
-    <Link to={`/edit/ticket/${ticket.id}`}
-    className="view-ticket-button"><button id="inner-button">Edit</button></Link>
-    <div className="ticket-delete-button">
-     <OpenModalMenuItem 
-                    
-                    itemText="Delete"
-                     modalComponent={<DeleteTicket />}
-                    />
+    if (Object.keys(customer).length === 0 || !customer) return null
+    if (!ticket) return null
+    if (!user) return null
+    if (!macros) return null
+
+    // console.log("MACRO", macros)
+    let chosenMacro;
+    if (ticket.apply_macro) {
+
+        macros ? chosenMacro = macros.macros.find((macro) => macro.id === ticket.apply_macro) : null
+    } else {
+        chosenMacro = "No Macro Chosen"
+    }
+    // console.log("CHOSEN MAC", chosenMacro)
+    return (
+        <div id="view-ticket-form">
+            <div id="view-ticket-top">
+                <div id="view-ticket-left">
+                    <label>
+                        <h4>Requester</h4>
+
+                        <input
+                            className="view-ticket-requester"
+                            disabled={true}
+                            value={customer.customer ? customer.customer.name : null}
+                        />
+                    </label>
+                    <p className="error-message"></p>
+                    <label>
+                        <h4> Assignee</h4>
+
+                        <input
+                            className="view-ticket-assignee"
+                            disabled={true}
+                            value={user.username}
+                        />
+                    </label>
+                    <div id="view-type-priority">
+                        <label id="type-label">
+                            <h4>Type</h4>
+
+                            <input
+                                className="view-ticket-type"
+                                disabled={true}
+                                value={ticket.type}
+                            />
+                        </label>
+                        <lable id="priority-label">
+                            <h4>Priority</h4>
+
+                            <input
+                                className="view-ticket-priority"
+                                disabled={true}
+                                value={ticket.priority}
+                            />
+                        </lable>
                     </div>
-    </div>
-   
-    </div>
+                </div>
+                <div id="view-ticket-middle">
+                    <label>
+                        <h4>Title</h4>
 
-</div>
-)
+                        <input
+                            id="view-ticket-title-input"
+                            value={ticket.title}
+                            disabled={true}
+                        />
+                    </label>
+                    <p className="error-message"> </p>
+                    <lable> </lable>
+                    <label>
+
+                        <h4>Description</h4>
+                        <input
+                            id="view-ticket-description-input"
+                            disabled={true}
+                            value={ticket.description}
+                        />
+                    </label>
+                    <p className="error-message"> </p>
+                </div>
+                <div id="view-ticket-right">
+                    <label>
+                        <h4 id="img-label">Image</h4>
+
+                        <img
+                            id="view-ticket-image"
+                            src={ticket.tickets_images[0] ? ticket.tickets_images[0].image : null} />
+                    </label>
+                </div>
+            </div>
+            <div id="view-ticket-bottom">
+                <lable className="view-ticket-macro">
+                    <h4>Apply Macro</h4>
+
+                    <select
+                        id="view-macros-input"
+                        disabled={true}
+                        value={chosenMacro ? chosenMacro.id : null}
+                    >
+                        <option>{chosenMacro ? chosenMacro.description : null}</option>
+                    </select>
+                </lable>
+                <div className="view-delete-edit-buttons">
+                    <Link to={`/edit/ticket/${ticket.id}`}
+                        className="view-ticket-button"><button id="inner-button">Edit</button></Link>
+                    <div className="ticket-delete-button">
+                        <OpenModalMenuItem
+
+                            itemText="Delete"
+                            modalComponent={<DeleteTicket />}
+                        />
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default ViewTickets
